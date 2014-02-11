ea6001cc4519f3900753e11c58c574227b9061aa
Dennis <danfossi@itfor.it>
2014-02-11 12:48:47 +0100

    Update enumpc.ps1
    * Better output align

--- enumpc.ps1
+++ enumpc.ps1
@@ -1,7 +1,7 @@
 # description		: Script to enumerate local machine
 # author(s)		: Dennis Anfossi
-# date			: 01.02.2014
-# version		: 0.1.1
+# date			: 11.02.2014
+# version		: 0.1.2
 # license		: GPLv2
 # usage			: powershell -Noexit <path>\<to>\<script>.ps1
 #			: powershell <path>\<to>\<script>.ps1 | out-file -filepath "C:\outfile.log"
@@ -29,15 +29,15 @@ else
 
 "== System Info == "
 "=== OS === "
-"             * OS Version  : " + (Get-WmiObject -class Win32_OperatingSystem).Caption
-"             * Installed on: " + ([WMI]'').ConvertToDateTime((Get-WmiObject Win32_OperatingSystem).InstallDate) 
+"* OS Version  : " + (Get-WmiObject -class Win32_OperatingSystem).Caption
+"* Installed on: " + ([WMI]'').ConvertToDateTime((Get-WmiObject Win32_OperatingSystem).InstallDate) 
 if ($win_is_compatible -match "True"){
-"             * Architecture: " + (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+"* Architecture: " + (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 }
 		$lastboot = Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
-"             * Last boot   : " + $lastboot.lastbootuptime
+"* Last boot   : " + $lastboot.lastbootuptime
 if ($win_is_compatible -match "True"){
-"             * PowerShell  : " + (Get-ExecutionPolicy)
+"* PowerShell  : " + (Get-ExecutionPolicy)
 }
 " "
 
@@ -48,22 +48,22 @@ if ($win_is_compatible -match "True"){
 
 "=== CPU(s) === "
 $cpu = Get-WmiObject -class win32_processor
-"             * CPU Type    : " + $($cpu.caption)
-"             * CPU Speed   : " + $($cpu.CurrentClockSpeed) + " MHz"
+"* CPU Type    : " + $($cpu.caption)
+"* CPU Speed   : " + $($cpu.CurrentClockSpeed) + " MHz"
 " "
 
 "=== RAM === "
 $ram = Get-WmiObject -Class Win32_ComputerSystem
-"             * RAM         : " + ([math]::Round($ram.TotalPhysicalMemory / 1gb,2)) + "Gb"
+"* RAM         : " + ([math]::Round($ram.TotalPhysicalMemory / 1gb,2)) + "Gb"
 " "
 
 $disks = Get-WmiObject Win32_LogicalDisk
 "=== Disk(s) ==="
 foreach ($disk in $disks) {
-"            * " + $disk.DeviceID + " (S/N: " + $($disk.VolumeSerialNumber) + ")"
-"            ** FileSystem  : " + $($disk.FileSystem)
-"            ** Disk Size   : " + $([math]::Round($disk.size / 1gb,2)) + "Gb"
-"            ** Free space  : " + $([math]::Round($disk.freespace / 1gb,2)) + "Gb"
+"* " + $disk.DeviceID + " (S/N: " + $($disk.VolumeSerialNumber) + ")"
+"** FileSystem  : " + $($disk.FileSystem)
+"** Disk Size   : " + $([math]::Round($disk.size / 1gb,2)) + "Gb"
+"** Free space  : " + $([math]::Round($disk.freespace / 1gb,2)) + "Gb"
 " "
 }
 
@@ -72,19 +72,19 @@ foreach ($disk in $disks) {
 $strComputer ="."
 $colItems = Get-WmiObject Win32_NetworkAdapterConfiguration -Namespace "root\CIMV2" | where{$_.IPEnabled -eq “True”}
 foreach($objItem in $colItems) {
-    " * Interface: " + $($objItem.Description)
-	"            ** IP Address  : " + $($objItem.IPAddress[0])
-	"            ** Netmask     : " + $($objItem.IPSubnet)
-	"            ** Gateway     : " + $($objItem.DefaultIPGateway)
-	"            ** DNS Server  : " + $($objItem.DNSServerSearchOrder)
-	"            ** Domain      : " + $($objItem.DNSDomain)
-	" "
-	"            ** Mac Address : " + $($objItem.MACAddress)
-	" "
-    	"            ** DHCP Enabled: " + $($objItem.DHCPEnabled)
-	"            ** DHCP Server : " + $($objItem.DHCPServer)
+"* Interface: " + $($objItem.Description)
+"** IP Address  : " + $($objItem.IPAddress[0])
+"** Netmask     : " + $($objItem.IPSubnet)
+"** Gateway     : " + $($objItem.DefaultIPGateway)
+"** DNS Server  : " + $($objItem.DNSServerSearchOrder)
+"** Domain      : " + $($objItem.DNSDomain)
+" "
+"** Mac Address : " + $($objItem.MACAddress)
+" "
+"** DHCP Enabled: " + $($objItem.DHCPEnabled)
+"** DHCP Server : " + $($objItem.DHCPServer)
 
-	" "		  
+" "		  
 }
 
 "== Account Info =="
@@ -108,17 +108,17 @@ foreach ($Computer in $ComputerName) {
 $net = New-Object -comobject Wscript.Network
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $WindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($CurrentUser)
-"             * Username   : "  + $($net.username)
+"* Username   : "  + $($net.username)
 if ($WindowsPrincipal.IsInRole("Administrators"))
 {
-    "             * Group      : Administators"
+"* Group      : Administators"
 }
 else
 {
-   "              * Group      : Users"
+"* Group      : Users"
 }
 	if ($win_is_compatible -match "True"){
-"             * UAC        : " + (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).EnableLUA
+"* UAC        : " + (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).EnableLUA
 }
 " "
 
@@ -128,7 +128,7 @@ if ($netItems){
 "== Mapped Drive =="
 
 foreach($mapItem in $netItems) {
-    "             * " +$($mapItem.Name) + "         : " + $($mapItem.ProviderName)
+"* " +$($mapItem.Name) + "         : " + $($mapItem.ProviderName)
 }
 " "
 }
@@ -136,15 +136,15 @@ foreach($mapItem in $netItems) {
 "== Printers =="
 $printItems = Get-WMIObject -class Win32_Printer | Select Name,DriverName,PortName
 foreach($instPrinter in $printItems) {
-"             * " + $($instPrinter.PortName)
-"             ** " + $($instPrinter.name) + ": " + $($instPrinter.DriverName )
+"* " + $($instPrinter.PortName)
+"** " + $($instPrinter.name) + ": " + $($instPrinter.DriverName )
 " "
 }
 
 "== Installed Software =="
 $listsoft = Get-WmiObject -Class Win32_Product | Select-Object -Property Name | Sort-Object -Property Name
 foreach ($instsoft in $listsoft) {
-"             * " + $($instsoft.name)
+"* " + $($instsoft.name)
 }
 
 " "
